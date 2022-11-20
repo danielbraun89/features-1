733ea1711a1cc66649e20597b7b0fb76b109a16e
Daniel Braun <daniel.braun@outlook.co.il>
2022-11-20 17:19:02 +0000

    test

--- test/typescript1/test.sh
+++ test/typescript1/test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+ls /usr/local/share/nvm/versions/node/v18.12.1/bin/
 
 source dev-container-features-test-lib
+check "node --version" node --version
+check "npm --version" npm --version
 
 check "tsc --version" tsc --version
 
